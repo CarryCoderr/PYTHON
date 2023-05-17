6ed4cbf84ea532c7a7d937755300d63bbf0c5103
Carry Coder <carrycoderr@gmail.com>
2023-05-17 15:40:43 +0530

    while Loop

---
+++ Tutorial/Day9/day9.py
@@ -0,0 +1,15 @@
+count=0
+while count<3:
+    print("carrycoder")
+    count=count+1
+print("no")
+num=0
+while num<=10:
+    print(num)
+    num=num+1
+print('oh')
+str="carrycoder"
+i=0
+while i<len(str):
+    print(str[i])
+    i=i+1
