ea618ddb9010afa46615d15570290ef03d9defce
Carry Coder <carrycoderr@gmail.com>
2023-05-26 10:10:57 +0530

    While Assign.

---
+++ Tutorial/Day11/day11.py
@@ -0,0 +1,34 @@
+ja=25
+while ja<=60:
+    print(ja)
+    ja=ja+1
+else:
+    print("its time for rt")
+ta=1000
+while ta !=100:
+    print(ta)
+    ta=ta-100
+else:
+    print("ab paise dalo vara ghar vala marange")
+print("hello All")
+n=int(input('enter the number'))
+i=1
+while i<=n:
+    j=n
+    while j>=i:
+        print("*",end=" ")
+        j-=1
+    print()
+    i+=1
+###
+n1=int(input("enter the number"))
+k=1
+i=1
+while i<=n :
+    j=1
+    while j<=i:
+        print(k,end=" ")
+        j+=1
+        k+=1
+    print()
+    i+=1
