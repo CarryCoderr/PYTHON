29c6281565d341224e2560b3b3805161e7bb3120
Carry Coder <carrycoderr@gmail.com>
2023-05-31 14:13:04 +0530

    sample for loop

---
+++ Tutorial/Day13/day13.py
@@ -0,0 +1,12 @@
+count=1
+for i in "                       ":
+    if count<=10:
+        print(count)
+        count+=1
+num=int(input("enter the number"))
+for i in (1,2,3,4,5,6,7,8,9,10):
+    print(f"{num} x {i}={num*i}")
+    
+st="iitmadaras"
+for i in st:
+    print(i)
